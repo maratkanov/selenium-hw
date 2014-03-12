2c77d868cce1d95e3747c9c3b168b07c2960af57
maratkanov <alexwitcher@gmail.com>
2014-03-12 11:30:05 +0400

    fixed problem with package name

---
+++ src/test/java/tech_mail/requirements/Application.java
@@ -0,0 +1,14 @@
+package tech_mail.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {
+        }
+
+        public class SearchByMultipleKeywords {
+        }
+    }
+}
---
+++ src/test/java/tech_mail/steps/EndUserSteps.java
@@ -0,0 +1,38 @@
+package tech_mail.steps;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+import tech_mail.pages.DictionaryPage;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class EndUserSteps extends ScenarioSteps {
+
+    DictionaryPage dictionaryPage;
+
+    @Step
+    public void enters(String keyword) {
+        dictionaryPage.enter_keywords(keyword);
+    }
+
+    @Step
+    public void starts_search() {
+        dictionaryPage.lookup_terms();
+    }
+
+    @Step
+    public void should_see_definition(String definition) {
+        assertThat(dictionaryPage.getDefinitions()).contains(definition);
+    }
+
+    @Step
+    public void is_the_home_page() {
+        dictionaryPage.open();
+    }
+
+    @Step
+    public void looks_for(String term) {
+        enters(term);
+        starts_search();
+    }
+}
